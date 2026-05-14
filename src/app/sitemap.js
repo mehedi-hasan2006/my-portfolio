bb2be2b690ac13eec1590be2b95b4120ab4e7dee
mehedi-hasan2006 <mehedihasan811914@gmail.com>
2026-05-15 00:10:41 +0600

    sitemap file changed

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,43 +1,8 @@
-// app/sitemap.js
 export default function sitemap() {
-  const baseUrl = "https://mdmehedi.vercel.app";
-
   return [
     {
-      url: baseUrl,
+      url: "https://mdmehedi.vercel.app",
       lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#skills`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#projects`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#education`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.6,
     },
   ];
 }
